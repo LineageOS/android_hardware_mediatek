a30fcbda0de4a95bf24c915150cbd0bc052d9585
bengris32 <bengris32@protonmail.ch>
2023-05-27 20:33:11 +0100

    gnss: Register HIDL GNSS 2.1 implementation
    * The GNSS AIDL interface is not a complete replacement for
      HIDL GNSS yet, hence also register the "legacy" HIDL GNSS
      implementation to retain support.
    
    Signed-off-by: bengris32 <bengris32@protonmail.ch>
    Change-Id: Ie29cffa6f16a87944783f3c489fb15755635f7b2

--- aidl/gnss/service.cpp
+++ aidl/gnss/service.cpp
@@ -19,21 +19,27 @@
 #include <android-base/logging.h>
 #include <android/binder_manager.h>
 #include <android/binder_process.h>
+#include <android/hardware/gnss/2.1/IGnss.h>
 #include <hidl/HidlSupport.h>
 #include <hidl/HidlTransportSupport.h>
+#include <hidl/LegacySupport.h>
 #include <log/log.h>
 #include <pthread.h>
 #include "AidlGnss.h"
 
 using aidl::android::hardware::gnss::AidlGnss;
+using ::android::hardware::gnss::V2_1::IGnss;
 using ::android::OK;
 using ::android::sp;
+using ::android::status_t;
 using ::android::hardware::configureRpcThreadpool;
 using ::android::hardware::joinRpcThreadpool;
+using ::android::hardware::registerPassthroughServiceImplementation;
 
 int main() {
     ABinderProcess_setThreadPoolMaxThreadCount(1);
     ABinderProcess_startThreadPool();
+    configureRpcThreadpool(1, true /*callerWillJoin*/);
 
     std::shared_ptr<AidlGnss> gnssAidl = ndk::SharedRefBase::make<AidlGnss>();
     const std::string instance = std::string() + AidlGnss::descriptor + "/default";
@@ -41,6 +47,10 @@ int main() {
             AServiceManager_addService(gnssAidl->asBinder().get(), instance.c_str());
     CHECK_EQ(status, STATUS_OK);
 
+    status_t hidl_status = registerPassthroughServiceImplementation<IGnss>("default");
+    CHECK_EQ(hidl_status, OK);
+
+    joinRpcThreadpool();
     ABinderProcess_joinThreadPool();
 
     return EXIT_FAILURE;  // should not reach
