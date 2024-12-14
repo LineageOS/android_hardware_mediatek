02900b51ebd20a5ca0ec7ed8760fa1f756d30e9c
claxten10 <claxten10@gmail.com>
2024-12-14 14:22:23 +0000

    mediatek: Provide a dummy `libaedv`
    * On AOSP, MediaTek AEE is useless, but a few blobs depend on libaedv.
    * Provide a stub library that will provide only the required functions to
      ensure the blobs that depend on libaedv work without needing the whole
      proprietary AEE lib.
    
    test: - audio
    - camera
    - ril
    - other that depend on libaedv
    
    Signed-off-by: claxten10 <claxten10@gmail.com>
    Co-authored-by: Mashopy <eliasgheeraert@gmail.com>
    Change-Id: Ic9031bf040390777ffb707b3f6504dd04043fa27

---
+++ libaedv/aee.c
@@ -0,0 +1,28 @@
+//
+// SPDX-FileCopyrightText: The LineageOS Project
+// SPDX-License-Identifier: Apache-2.0
+//
+
+int aee_switch_ftrace(int status) {
+    return 0;
+}
+
+int aee_system_exception(const char* lib, const char* path, unsigned int options,
+                         const char* log_msg) {
+    return 0;
+}
+
+int aee_system_warning(const char* lib, const char* path, unsigned int options,
+                       const char* log_msg) {
+    return 0;
+}
+
+int aee_log_msg(const char* lib, const char* path, unsigned int options, const char* log_msg,
+                const char* ver) {
+    return 0;
+}
+
+int aee_modem_warning(const char* lib, const char* path, unsigned int options, const char* log_msg,
+                      const char* ver) {
+    return 0;
+}
