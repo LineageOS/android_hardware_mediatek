96cb1397dfa965c615c639c1775912d2ad8bede2
Giovanni Ricca <giovanniricca@proton.me>
2025-06-01 19:02:09 +0200

    interfaces: Generic-ify `hidl_package_root` name
    Change-Id: I85a185ef6abce31579f3e2cfdd3b02e9beabfef6

--- interfaces/updates-makefiles.sh
+++ interfaces/updates-makefiles.sh
@@ -3,4 +3,4 @@
 source $ANDROID_BUILD_TOP/system/tools/hidl/update-makefiles-helper.sh
 
 do_makefiles_update \
-  "vendor.mediatek.hardware.mtkpower:hardware/mediatek/interfaces/mtkpower"
+  "vendor.mediatek.hardware:hardware/mediatek/interfaces/hardware"
