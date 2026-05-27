1fd59e90e1f38e860ea9f9b0fe194c67a9f4bf3f
jimmyshiu <jimmyshiu@google.com>
2026-05-27 06:58:58 +0200

    libmtkperf_client: log clean up
    Remove ALOGD logs.
    
    Bug: 230205812
    Test: adb shell logcat -b all | grep libperf
    Change-Id: I4125afec526b76e3905e75c22fc2bfb555810fa8

--- libmtkperf_client/perfctl_vendor.c
+++ libmtkperf_client/perfctl_vendor.c
@@ -9,58 +9,35 @@
 #include <log/log.h>
 
 uint64_t earaNotifyCVJobBegin(uint64_t jobId, uint64_t jobPriority, uint64_t* result) {
-    ALOGD("[%s]: jobId: %lu, jobPriority: %lu, result: %p", __func__, jobId, jobPriority, result);
     return 0;
 }
 
 uint64_t earaNotifyCVJobEnd(uint64_t jobId, uint64_t timestamp, uint32_t* status) {
-    ALOGD("[%s]: jobId: %lu, timestamp: %lu, status: %p", __func__, jobId, timestamp, status);
     return 0;
 }
 
-void earaGetUsage(uint32_t param1, uint32_t* usage, uint32_t* status) {
-    ALOGD("[%s]: param1: %u, usage: %p, status: %p", __func__, param1, usage, status);
-}
+void earaGetUsage(uint32_t param1, uint32_t* usage, uint32_t* status) {}
 
-void earaNotifyJobBegin(uint32_t jobId, uint64_t jobData, int64_t* param3, int64_t* param4) {
-    ALOGD("[%s]: jobId: %u, jobData: %lu, param3: %p, param4: %p", __func__, jobId, jobData, param3,
-          param4);
-}
+void earaNotifyJobBegin(uint32_t jobId, uint64_t jobData, int64_t* param3, int64_t* param4) {}
 
 uint64_t earaNotifyJobEnd(uint32_t jobId, uint64_t timestamp, uint32_t* status, uint32_t flags,
                           int64_t* inputData, int64_t* outputData, uint64_t context,
                           int64_t* result) {
-    ALOGD("[%s]: jobId: %u, timestamp: %lu, status: %p, flags: %u, "
-          "inputData: %p, outputData: %p, context: %lu, result: %p",
-          __func__, jobId, timestamp, status, flags, inputData, outputData, context, result);
     return 0;
 }
 
-void fbcNotifyTouch(int touchEvent) {
-    ALOGD("[%s]: touchEvent: %d", __func__, touchEvent);
-}
+void fbcNotifyTouch(int touchEvent) {}
 
-void xgfGetCamApkPid(uint32_t pid, uint32_t apkId, uint32_t flag) {
-    ALOGD("[%s]: pid: %u, apkId: %u, flag: %u", __func__, pid, apkId, flag);
-}
+void xgfGetCamApkPid(uint32_t pid, uint32_t apkId, uint32_t flag) {}
 
-void xgfGetCamServerPid(void) {
-    ALOGD("[%s]: Get camera server PID", __func__);
-}
+void xgfGetCamServerPid(void) {}
 
-void xgfGetFPS(int32_t* currentFPS, int32_t* averageFPS) {
-    ALOGD("[%s]: currentFPS: %p, averageFPS: %p", __func__, currentFPS, averageFPS);
-}
+void xgfGetFPS(int32_t* currentFPS, int32_t* averageFPS) {}
 
-void xgfGetFstbActive(uint64_t status) {
-    ALOGD("[%s]: status: %lu", __func__, status);
-}
+void xgfGetFstbActive(uint64_t status) {}
 
-void xgfWaitFstbActive(void) {
-    ALOGD("%s called", __func__);
-}
+void xgfWaitFstbActive(void) {}
 
 uint32_t xgfGetCmd(uint64_t* param) {
-    ALOGD("[%s]: param: %p", __func__, param);
     return 0;
 }
--- libmtkperf_client/powerhalwrap_vendor.c
+++ libmtkperf_client/powerhalwrap_vendor.c
@@ -9,73 +9,58 @@
 #include <log/log.h>
 
 int PowerHal_Wrap_mtkPowerHint(int hint, int data) {
-    ALOGD("[%s]: hint:%d, data:%d", __func__, hint, data);
     return 0;
 }
 
 int PowerHal_Wrap_mtkCusPowerHint(int hint, int data) {
-    ALOGD("[%s]: hint:%d, data:%d", __func__, hint, data);
     return 0;
 }
 
 int PowerHal_Wrap_querySysInfo(unsigned int param, unsigned int data) {
-    ALOGD("[%s]: param:%d, data:%d", __func__, param, data);
     return 0;
 }
 
 int64_t PowerHal_Wrap_notifyAppState(const char* pname, const char* aname, unsigned int pid,
                                      int status, unsigned int uid) {
-    ALOGD("[%s]: pname:%s, aname:%s, pid:%d, status:%d, uid:%d", __func__, pname, aname, pid,
-          status, uid);
     return 0;
 }
 
 int PowerHal_Wrap_scnReg() {
-    ALOGD("%s called", __func__);
     return 0;
 }
 
 int PowerHal_Wrap_scnConfig() {
-    ALOGD("%s called", __func__);
     return 0;
 }
 
 int PowerHal_Wrap_scnUnreg() {
-    ALOGD("%s called", __func__);
     return 0;
 }
 
 int PowerHal_Wrap_scnEnable() {
-    ALOGD("%s called", __func__);
     return 0;
 }
 
 int PowerHal_Wrap_scnDisable() {
-    ALOGD("%s called", __func__);
     return 0;
 }
 
 int PowerHal_Wrap_scnUltraCfg() {
-    ALOGD("%s called", __func__);
     return 0;
 }
 
 int PowerHal_TouchBoost(int duration) {
-    ALOGD("[%s]: duration %d", __func__, duration);
     return 0;
 }
 
 int PowerHal_Wrap_setSysInfo(int type, const char* data) {
-    ALOGD("[%s]: type: %d, data: %s", __func__, type, data);
     return 0;
 }
 
 int PowerHal_Wrap_setSysInfoAsync(int type, const char* data) {
-    ALOGD("[%s]: type: %d, data: %s", __func__, type, data);
     return 0;
 }
 
 int PowerHal_Wrap_EnableMultiDisplayMode(int enable, int fps) {
-    ALOGD("[%s]: enable: %d, fps: %d", __func__, enable, fps);
     return 0;
 }
