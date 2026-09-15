085a2f7b2fd20505861fa7ba44cba3382b0d5c37
Mashopy <eliasgheeraert@gmail.com>
2026-09-15 03:47:13 +0200

    frameworks: Introduce a minimal mediatek-telecom-common JAR
    Change-Id: Ifc0ef53a18515ba8bac9354d1e1ba85ca81a35cf

--- frameworks/mediatek-frameworks.mk
+++ frameworks/mediatek-frameworks.mk
@@ -6,8 +6,10 @@
 
 PRODUCT_PACKAGES += \
     mediatek-common \
+    mediatek-telecom-common \
     mediatek-telephony-common
 
 PRODUCT_BOOT_JARS += \
     mediatek-common \
+    mediatek-telecom-common \
     mediatek-telephony-common
