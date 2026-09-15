7ba30e1d388685885a0485a4bee5e17ef09159bd
Mashopy <eliasgheeraert@gmail.com>
2026-09-15 03:38:14 +0200

    frameworks: Introduce a minimal mediatek-telephony-common JAR
    After further inspection, only some parts of mediatek-telephony-common
    are used by mediatek-ims-extension-plugin JAR. This also avoids keeping
    a full JAR that is prone to dex2oat issues during compilation.
    
    Change-Id: Ie93e270943a9782491025218a37d7b1fe6ad25cf

---
+++ frameworks/telephony-common/src/com/mediatek/internal/telephony/imsphone/MtkImsPhoneCallTracker.java
@@ -0,0 +1,13 @@
+/*
+ * SPDX-FileCopyrightText: 2026 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.mediatek.internal.telephony.imsphone;
+
+import com.android.internal.telephony.imsphone.ImsPhoneCallTracker;
+import com.android.internal.telephony.imsphone.ImsPullCall;
+
+public class MtkImsPhoneCallTracker extends ImsPhoneCallTracker implements ImsPullCall {
+    public static final int IMS_SESSION_MODIFY_OPERATION_FLAG = 0x8000;
+}
